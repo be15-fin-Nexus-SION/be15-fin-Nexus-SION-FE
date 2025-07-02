bb0524b77d421f39a24b22b1b5f9e8e2e8ec2197
didiha <hcring@naver.com>
2025-07-03 00:41:08 +0900

    feat: 고객사 관리 페이지 구현을 위한 api 연결, router 설정, 사이드바 주소 수정

--- src/api/admin.js
+++ src/api/admin.js
@@ -41,3 +41,19 @@ export function addTechStack(techStackName) {
 export function deleteTechStack(name) {
   return api.delete(`/tech-stack/${encodeURIComponent(name)}`);
 }
+
+export function getAllClients() {
+  return api.get("/client-companies");
+}
+
+export function addClient(data) {
+  return api.post("/client-companies", data);
+}
+
+export function updateClient(clientCode, data) {
+  return api.patch(`/client-companies/${clientCode}`, data);
+}
+
+export function deleteClient(clientCode) {
+  return api.delete(`/client-companies/${clientCode}`);
+}
