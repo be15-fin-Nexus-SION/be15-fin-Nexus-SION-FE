d80c578d7d62b5272937c7ec0d4b9b84baba7212
sezeme <stellaa223@gmail.com>
2025-07-10 14:55:06 +0900

    feat: 알림 모달 구현

---
+++ src/features/notification/components/NotificationList.vue
@@ -0,0 +1,30 @@
+<script setup>
+import NotificationItem from "@/features/notification/components/NotificationItem.vue";
+
+const { notifications, isModalOpen } = defineProps({
+  notifications: {
+    type: Array,
+    required: true,
+  },
+  isModalOpen: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["close"]);
+</script>
+
+<template>
+  <div class="space-y-4 max-height overflow-y-auto">
+    <NotificationItem
+      @close="emit('close')"
+      v-for="notification in notifications"
+      :key="notification.notificationId"
+      :notification="notification"
+      :is-modal-open="isModalOpen"
+    />
+  </div>
+</template>
+
+<style scoped></style>
