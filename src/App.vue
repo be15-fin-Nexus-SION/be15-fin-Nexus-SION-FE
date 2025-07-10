a6f9ab08228b6955840fb207929b7074700ef1c8
develup_psy <develup_psy@github.com>
2025-07-10 20:50:27 +0900

    feat: 스쿼드 상세조회/스쿼드 일반조회/직무풀 조회/스쿼드 등록에서 개발자 상세 조회 모달 폼으로 뜨도록 구현

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import DeveloperDetailModal from "@/features/squad/components/modal/DeveloperDetailModal.vue";
+</script>
 
 <template>
   <router-view />
+  <DeveloperDetailModal />
 </template>
 
 <style scoped></style>
