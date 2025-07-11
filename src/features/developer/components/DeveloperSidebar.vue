daaa3f0d58116a7d16ef79024eeb4725f1743324
kihongkim <clevermind2@naver.com>
2025-07-11 16:56:42 +0900

    개발자 사이드바 설정

---
+++ src/features/developer/components/DeveloperSidebar.vue
@@ -0,0 +1,20 @@
+<template>
+  <aside
+    class="min-w-[230px] max-w-[230px] min-h-screen bg-white border-r border-[#EEEEEE] flex flex-col justify-between items-start p-4"
+  >
+    <div class="flex flex-col gap-4 w-full">
+      <SidebarItem
+        v-for="(item, index) in items"
+        :key="index"
+        :label="item.label"
+        :highlighted="item.highlighted"
+        :to="item.to"
+      />
+    </div>
+  </aside>
+</template>
+
+<script setup>
+import SidebarItem from "@/components/side/SidebarItem.vue";
+defineProps({ items: Array });
+</script>
